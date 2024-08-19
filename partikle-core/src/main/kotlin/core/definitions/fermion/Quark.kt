bdb51979796c6fb1633caffdf9abee84890335df
Kid A <me@thisismeamir.com>
2024-08-19 20:11:15 +0330

    [Implementation] Quark and QuarkType

---
+++ partikle-core/src/main/kotlin/core/definitions/fermion/Quark.kt
@@ -0,0 +1,23 @@
+package core.definitions.fermion
+
+import core.definitions.attrs.ColorCharge
+import core.definitions.attrs.ElectricCharge
+import core.definitions.attrs.Generation
+
+data class Quark<QuarkType>(
+    override val type: QuarkType,
+    override val mass: Double,
+    override val electricCharge: ElectricCharge,
+    override val colorCharge: ColorCharge,
+    override val generation: Generation,
+) : Fermion<QuarkType> {
+    init {
+        assert(
+          electricCharge != ElectricCharge.Zero
+                && electricCharge != ElectricCharge.NegativeOne
+                && electricCharge != ElectricCharge.PositiveOne
+        ) {
+            "A Quark does not possess integer electric charge!"
+        }
+    }
+}
---
+++ partikle-core/src/main/kotlin/core/definitions/fermion/QuarkType.kt
@@ -0,0 +1,23 @@
+package core.definitions.fermion
+
+
+enum class QuarkType {
+    // Quarks
+    Up,
+    AntiUp,
+
+    Down,
+    AntiDown,
+
+    Charm,
+    AntiCharm,
+
+    Strange,
+    AntiStrange,
+
+    Top,
+    AntiTop,
+
+    Bottom,
+    AntiBottom
+}
