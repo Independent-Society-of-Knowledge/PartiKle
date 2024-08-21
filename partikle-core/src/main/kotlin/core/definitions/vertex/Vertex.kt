3b06774527529a17bde9c9d900b096da8336aedb
Kid A <thisismeamir@outlook.com>
2024-08-21 19:43:41 +0330

    [Implementation] Vertex Interface for generic vertices.

--- partikle-core/src/main/kotlin/core/definitions/vertex/Vertex.kt
+++ partikle-core/src/main/kotlin/core/definitions/vertex/Vertex.kt
@@ -1,18 +1,24 @@
 package core.definitions.vertex
 
-import core.definitions.boson.Boson
 import core.definitions.boson.BosonType
-import core.definitions.fermion.Fermion
-import core.definitions.fermion.LeptonType
-import core.definitions.fermion.QuarkType
+import core.definitions.fermion.FermionType
 import core.utils.idGen
-import java.util.concurrent.atomic.AtomicLong
 
 interface Vertex {
     val id: Long
         get() = idGen.incrementAndGet()
     val type: VertexType
-    val allowedLeptons: List<LeptonType>
-    val allowedQuarks: List<QuarkType>
-    val allowedBosons: List<BosonType>
+
+    val fermions: List<FermionType>
+    val bosons: List<BosonType>
+
+    fun interactiveFor(particleType: FermionType): Boolean {
+        return particleType in fermions
+    }
+
+    fun byBoson(bosonType: BosonType): Boolean {
+        return bosonType in bosons
+    }
+
 }
+
